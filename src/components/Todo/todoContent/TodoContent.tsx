37655bb125179203dfb825ed3f8a255a13fddcce
seongjin <kssj9056@gmail.com>
2023-04-10 03:20:59 +0900

    ✨ Feat : 투두 수정 기능구현

--- src/components/Todo/todoContent/TodoContent.tsx
+++ src/components/Todo/todoContent/TodoContent.tsx
@@ -6,19 +6,33 @@ import EditIcon from '@mui/icons-material/Edit';
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
 import Checkbox from "@mui/material/Checkbox";
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import Input from '@mui/material/Input';
 import { Todo } from "../TodoList/TodoList";
 import { UpdateAxios, deleteTodoAxios } from "../../../api/todo";
-
-
+import { useInput } from "../../../hooks/useInput";
+import { dataIdInputPropsType } from "../../../types/inputData";
 
 type PropsType = {
   item: Todo;
   getTodos: () => Promise<void>
 }
 
+type EditInputPropsType = dataIdInputPropsType & {
+  'aria-label' : string;
+}
+
 const TodoContent = ({ item,getTodos }: PropsType) => {
+  
   const [checked, setChecked] = useState(item.isCompleted);
+  const [edit, setEdit] = useState(false);
+  const [editText, editTextChange] = useInput(item.todo);
+  const editInputProps : EditInputPropsType = {
+    "data-testid" : "modify-input",
+    'aria-label': 'editInput'
+  }
 
   // 완료여부 수정
   const handleCheckToggle = () => {
@@ -32,19 +46,38 @@ const TodoContent = ({ item,getTodos }: PropsType) => {
       return
     }
     await deleteTodoAxios(item.id)
-    getTodos();
+    await getTodos();
   }
+
+  // 수정 취소, 수정 버튼
+  const handleEdit = () => {
+    setEdit(!edit)
+    editText.setInputValue(item.todo);
+
+  }
+  // 수정 완료 버튼
+  const editComplete = async () => {
+    if(!window.confirm('수정하시겠습니까?')){
+      return
+    }
+
+   await UpdateAxios(item.id, editText.inputValue,checked)
+   await getTodos();
+    setEdit(!edit)
+}
+
    
     return (
+      <>
+      {edit ?
         <TodoLi
-        textline = { checked ? 'line-through' : 'initial'}
         secondaryAction={
           <>
-            <IconButton data-testid="modify-button" edge="end" aria-label="modificationBtn">
-              <EditIcon/>
+            <IconButton data-testid="submit-button" onClick={editComplete} edge="end" aria-label="CompletedBtn">
+              <TaskAltIcon/>
             </IconButton>
-            <IconButton onClick={toDoDelete} data-testid="delete-button" edge="end" aria-label="deleteBtn">
-              <DeleteOutlineRoundedIcon/>
+            <IconButton data-testid="cancel-button" onClick={handleEdit} edge="end" aria-label="cancelBtn">
+              <HighlightOffIcon/>
             </IconButton>
           </>
         }
@@ -61,13 +94,48 @@ const TodoContent = ({ item,getTodos }: PropsType) => {
                     checked={checked}
                     tabIndex={-1}
                     disableRipple
-                    inputProps={{ "aria-labelledby": `${item.id}` }}
+                    inputProps={{ "aria-labelledby" : `${item.id}` }}
                     onClick={handleCheckToggle}
                 />
             </ListItemIcon>
-            <ListItemText id={`${item.id}`} primary={`${item.todo}`} />
+            <Input value={editText.inputValue} onChange={editTextChange} inputProps={editInputProps} />
         </ListItemButton>
-    </TodoLi> 
+    </TodoLi>
+    :
+    <TodoLi
+    textline = { checked ? 'line-through' : 'initial'}
+    secondaryAction={
+      <>
+        <IconButton onClick={handleEdit} data-testid="modify-button" edge="end" aria-label="modificationBtn">
+          <EditIcon/>
+        </IconButton>
+        <IconButton onClick={toDoDelete} data-testid="delete-button" edge="end" aria-label="deleteBtn">
+          <DeleteOutlineRoundedIcon/>
+        </IconButton>
+      </>
+    }
+    
+    disablePadding
+>
+    <ListItemButton
+        role={undefined}
+        dense
+    >
+        <ListItemIcon>
+            <Checkbox
+                edge="start"
+                checked={checked}
+                tabIndex={-1}
+                disableRipple
+                inputProps={{ "aria-labelledby": `${item.id}` }}
+                onClick={handleCheckToggle}
+            />
+        </ListItemIcon>
+        <ListItemText id={`${item.id}`} primary={`${item.todo}`} />
+    </ListItemButton>
+  </TodoLi> 
+      }
+      </>
     );
 };
 
